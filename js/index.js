c2130beeabcb09c731e6e4705341976039e9ef6b
Alejandro <alejandrocs402@gmail.com>
2022-10-18 16:00:38 +0100

    captacion de arrows y eliminacion de notas al fallar y acertar

--- js/index.js
+++ js/index.js
@@ -1,28 +1,134 @@
-// TODO
-// var pos = ['630px', '825px', '1030px', '1223px']
-
-var Nota = {
-  html: document.createElement('div'),
-  top: 10,
-  height: 70, 
-  incremento: 1,
-  timerId: null,
-  move() {
-    if (this.top == 728) {
-      guitarra.removeChild(this.html)
-    }
+function Nota(left) {
+  this.left = left
+  this.top = 10
+  this.height = 70
+  this.incremento = 1
+  this.timerId = null
+  this.html = document.createElement('div')
+  this.move = function () {
     this.top += this.incremento
     this.html.style.top = this.top + 'px'
-  },
-  start() {
-    this.timerId = setInterval(this.move.bind(this), 10)
   }
 }
-Nota.html.setAttribute('class', 'notaCSS');
-var guitarra = document.getElementById('guitarra')
-guitarra.appendChild(Nota.html)
+var arrayLeft = []
+var arrayUp = []
+var arrayDown = []
+var arrayRight = []
 
-Nota.start()
+function generator() {
+  var position = ['629px', '835px', '1043px', '1248px']
+  var ranPosition = Math.floor(Math.random() * 4)
+  var newNota = new Nota(position[ranPosition])
+  newNota.html.setAttribute('class', 'notaCSS');
+  newNota.html.style.left = newNota.left
+  var guitarra = document.getElementById('guitarra')
+  guitarra.appendChild(newNota.html)
+
+  if (ranPosition === 0) {
+    arrayLeft.push(newNota)
+  }
+  if (ranPosition === 1) {
+    arrayUp.push(newNota)
+  }
+  if (ranPosition === 2) {
+    arrayDown.push(newNota)
+  }
+  if (ranPosition === 3) {
+    arrayRight.push(newNota)
+  }
+
+}
+var timerGame = null
+var timerGenerator = null
+
+function moveNote() {
+  arrayLeft.forEach(function (note) {
+    note.move()
+  }.bind(this))
+  arrayUp.forEach(function (note) {
+    note.move()
+  }.bind(this))
+  arrayDown.forEach(function (note) {
+    note.move()
+  }.bind(this))
+  arrayRight.forEach(function (note) {
+    note.move()
+  }.bind(this))
+}
+
+function removeNote() {
+  arrayLeft.forEach(function (note) {
+    if (note.top === 728) {
+      guitarra.removeChild(note.html)
+      arrayLeft.shift()
+    }
+  }.bind(this))
+  arrayUp.forEach(function (note) {
+    if (note.top === 728) {
+      guitarra.removeChild(note.html)
+      arrayUp.shift()
+    }
+  }.bind(this))
+  arrayDown.forEach(function (note) {
+    if (note.top === 728) {
+      guitarra.removeChild(note.html)
+      arrayDown.shift()
+    }
+  }.bind(this))
+  arrayRight.forEach(function (note) {
+    if (note.top === 728) {
+      guitarra.removeChild(note.html)
+      arrayRight.shift()
+    }
+  }.bind(this))
+}
+
+function checkNote(pressed) {
+  switch (pressed) {
+    case 'ArrowLeft':
+      arrayLeft.forEach(function (note) {
+        if (note.top > 582 && note.top + note.height < 712) {
+          guitarra.removeChild(note.html)
+          arrayLeft.shift()
+        }
+      }.bind(this))
+      break;
+    case 'ArrowUp':
+      arrayUp.forEach(function (note) {
+        if (note.top > 582 && note.top + note.height < 712) {
+          guitarra.removeChild(note.html)
+          arrayUp.shift()
+        }
+      }.bind(this))
+      break;
+    case 'ArrowDown':
+      arrayDown.forEach(function (note) {
+        if (note.top > 582 && note.top + note.height < 712) {
+          guitarra.removeChild(note.html)
+          arrayDown.shift()
+        }
+      }.bind(this))
+      break;
+    case 'ArrowRight':
+      arrayRight.forEach(function (note) {
+        if (note.top > 582 && note.top + note.height < 712) {
+          guitarra.removeChild(note.html)
+          arrayRight.shift()
+        }
+      }.bind(this))
+      break;
+  }
+}
+
+function startGame() {
+  timerGenerator = setInterval(generator, 2000)
+  timerGame = setInterval(function () {
+    moveNote()
+    removeNote()
+  }, 10)
+}
+
+
 
 const arrows = {
   sprites: document.getElementsByClassName("botn"),
@@ -36,6 +142,7 @@ const arrows = {
     switch (pressed) {
       case 'ArrowLeft':
         this.colorAndTransparent(0, 'red');
+        checkNote(pressed)
         break;
       case 'ArrowUp':
         this.colorAndTransparent(1, 'green');
@@ -47,9 +154,6 @@ const arrows = {
         this.colorAndTransparent(3, 'yellow');
         break;
     }
-    if (Nota.top > 582 && Nota.top+Nota.height < 712) {
-      console.log('DENTRO PERO CUALQUIER TECLA')
-    }
   }
 }
 
@@ -58,14 +162,4 @@ window.addEventListener("keydown", function (e) {
     arrows.activatedArrow(e.code);
   }
 })
-
-// var notaCSS = {
-//     top: 10px;
-//     left: 630px;
-//     left2: 825px;
-//     left3: 1030px;
-//     left4: 1223px;
-//     height: '200px',
-//     width: '200px',
-//     backgroundColor: 'green',
-// }
+startGame()
